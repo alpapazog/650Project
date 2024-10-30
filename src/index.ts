08bc7bee459181fd170b951294c32ec2d153b4c3
alpapazog <alpapazog@gmail.com>
2024-10-31 00:16:05 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from TypeScript Lambda!" }),
+  };
+};
